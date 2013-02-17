6c45cacded128d2b99c1f70c9ffd0259f87cddd1
Richard Tsai <dawning123@gmail.com>
2013-02-17 18:38:04 +0800

    解决在地图未初始化时无法收起列表的问题

--- static/script/loadmap.js
+++ static/script/loadmap.js
@@ -245,26 +245,30 @@ $(document).ready(function(){
 
 //控制侧边栏
 function toggleSide() {
-  if($('#showRoute').hasClass('active')) {
-    if(!gHideSide) {//隐藏侧边栏
-      $('.container-fluid div.span9').removeClass('span9').addClass('span11');
+  if(!gHideSide) {//隐藏侧边栏
+    $('.container-fluid div.span9').removeClass('span9').addClass('span11');
+
+    if(typeof(map) != 'undefined' && map != null)
       google.maps.event.trigger(map, 'resize');
-      $('.container-fluid div.span3').
-        css('position', 'absolute').
-        animate({left: -$('.container-fluid div.span3').width()}, 400,
-                function() {
-                  $('#showSide').removeClass('hide');
-                  gHideSide = true;
-                }
-               );
-    }
-    else {//显示侧边栏
-      $('.container-fluid div.span11').removeClass('span11').addClass('span9');
+
+    $('.container-fluid div.span3').
+      css('position', 'absolute').
+      animate({left: -$('.container-fluid div.span3').width()}, 400,
+              function() {
+                $('#showSide').removeClass('hide');
+                gHideSide = true;
+              }
+             );
+  }
+  else {//显示侧边栏
+    $('.container-fluid div.span11').removeClass('span11').addClass('span9');
+
+    if(typeof(map) != 'undefined' && map != null)
       google.maps.event.trigger(map, 'resize');
-      $('.container-fluid div.span3').css('position', 'static');
-      $('#showSide').addClass('hide');
-      gHideSide = false;
-    }
+
+    $('.container-fluid div.span3').css('position', 'static');
+    $('#showSide').addClass('hide');
+    gHideSide = false;
   }
 }
 
